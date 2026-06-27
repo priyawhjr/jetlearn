d674514280a1c6f77906fb9b6fa8c7ab87fdcfed
priyawhjr <132220332+priyawhjr@users.noreply.github.com>
2026-06-27 21:31:07 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+#Create a dictionary
+sample_dict = {
+  "name": "Pulkit",
+  "age": 23,
+  "city": "Agra",
+}
+
+# print dict
+print(sample_dict)
+
+# Accessing values in a dictionary
+print(sample_dict["name"],'\n')
+print(sample_dict,'\n')
+
+# Create a list with the same information to show the difference between list and a dictionary
+sample_list = ["Pulkit", 23, "Agra"]
+print(f'Its a list item at index 0 : {sample_list[0]}')
